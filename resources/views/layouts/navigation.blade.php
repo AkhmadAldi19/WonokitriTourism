cab4c3b069d9d68db20f8d70d969406e17c1fe2b
AkhmadAldi19 <103039058+AkhmadAldi19@users.noreply.github.com>
2023-11-13 12:35:02 +0700

    comment navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -36,14 +36,14 @@
                     </p>
                 </a>
             </li>
-            <li class="nav-item">
+            {{-- <li class="nav-item">
                 <a href="{{ route('admin.bookings.index') }}" class="nav-link">
                     <i class="nav-icon fas fa-date"></i>
                     <p>
                         {{ __('validate') }}
                     </p>
                 </a>
-            </li>
+            </li> --}}
 
             <li class="nav-item">
                 <a href="{{ route('admin.travel_packages.index') }}" class="nav-link">
